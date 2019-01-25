8ac0a37a992677b651becc1d7eba3018aa168074
Sheila <s.hopper01110@gmail.com>
2019-01-25 12:56:53 -0500

    React-Smurfs-initial

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,7 +14,9 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios
+    .post(`http://localhost.3333/smurfs`, this.state.smurfs)
+    .then(res => {
     this.setState({
       name: '',
       age: '',
